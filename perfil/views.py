4bd66979f6e90d419c9c377ae063bfc2805a040b
Richard Moliner Júnior <111286822+RichardMoliner@users.noreply.github.com>
2023-07-05 16:54:46 -0300

    Conexão do template com model para exibição das contas cadastradas

--- perfil/views.py
+++ perfil/views.py
@@ -8,7 +8,15 @@ def home(request):
   return render(request, 'home.html')
 
 def gerenciar(request):
-  return render(request, 'gerenciar.html')
+  contas = Conta.objects.all()
+  total_conta = 0
+
+  for conta in contas:
+    total_conta += conta.valor
+
+  return render(request, 'gerenciar.html',
+                {'contas': contas,
+                 'total_conta': total_conta})
 
 def cadastrar_banco(request):
   apelido = request.POST.get('apelido')
